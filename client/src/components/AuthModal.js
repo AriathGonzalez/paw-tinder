d6719e53c7da3ac53bb47bb139bf47eb14af1fa5
AriathGonzalez <84883360+AriathGonzalez@users.noreply.github.com>
2024-06-22 21:10:18 -0600

    Removed module type

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,6 +1,6 @@
 import "../styles/AuthModal.css";
 import { useState } from "react";
-import axios from "axios";
+import { axios } from "axios";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
